39c09b773d5db20947c5a78280b9b75275cf55ab
João Senna Radicchi <joaoradicchi@gmail.com>
2024-03-12 17:24:09 -0600

    whatever

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+import fs from "fs";
+import path from "path";
+import jsonServer from "json-server";
+
+const db = JSON.parse(fs.readFileSync(path.join("db.json")));
+
 // JSON Server module
-const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(db);
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
@@ -9,15 +14,18 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/*": "/$1",
- })
+  // Add custom route here if needed
+  jsonServer.rewriter({
+    "/*": "/$1",
+  })
 );
 server.use(router);
+server.post("/students", (req, res) => {
+  res.jsonp(req.body);
+});
 // Listen to port
 server.listen(3000, () => {
- console.log("JSON Server is running");
+  console.log("JSON Server is running");
 });
 
 // Export the Server API
