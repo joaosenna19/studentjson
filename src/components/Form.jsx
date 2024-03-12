8e25346d789ebbdc46bb44a5a5190bed36854cbc
Diogo Carbone <diogo.carbone@gmail.com>
2024-03-11 22:14:16 -0600

    removed the ' that was bugging the deployment

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -39,20 +39,20 @@ const Form = () => {
     <form className="flex flex-col mt-5" onSubmit={handleSubmit}>
       <FormField
         htmlFor={"first_name"}
-        title={"Student's First Name"}
+        title={" First Name"}
         type={"text"}
         id={"first_name"}
-        placeholder={"Enter student's first name"}
+        placeholder={"Enter  first name"}
         value={newStudent.first_name}
         onChange={handleChange}
         classes={formFieldClasses}
       />
       <FormField
         htmlFor={"last_name"}
-        title={"Student's Last Name"}
+        title={" Last Name"}
         type={"text"}
         id={"last_name"}
-        placeholder={"Enter student's last name"}
+        placeholder={"Enter  last name"}
         value={newStudent.last_name}
         onChange={handleChange}
         classes={formFieldClasses}
@@ -60,7 +60,7 @@ const Form = () => {
 
       <FormField
         htmlFor={"dob"}
-        title={"Student's DOB"}
+        title={" DOB"}
         type={"date"}
         id={"dob"}
         value={newStudent.dob}
@@ -71,7 +71,7 @@ const Form = () => {
       />
       <FormField
         htmlFor={"grade"}
-        title={"Student's Grade"}
+        title={" Grade"}
         type={"number"}
         id={"grade"}
         value={newStudent.grade}
